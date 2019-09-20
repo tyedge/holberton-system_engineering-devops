d725b31f70718a6c5f0304e9152e9d445171ce8b
tyedge <tyedge.inc@gmail.com>
2019-09-20 01:59:31 +0000

    Adding Task0 files

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This module queries the Reddit API and returns the number of subscribers"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """ This function returns the number of subscribers """
+    header = {'user-agent': 'Agenty'}
+    red_resp = requests.get('https://www.reddit.com/r/{}/about.\
+    json'.format(subreddit), headers=header, allow_redirects=False)
+
+    if red_resp.status_code >= 300:
+        return 0
+    return (red_resp.json().get('data').get('subscribers'))
