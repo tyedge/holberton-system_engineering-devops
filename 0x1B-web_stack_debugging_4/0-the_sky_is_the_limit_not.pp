f5a27047ac985878432d1d7a53204a08bab23328
tyedge <tyedge.inc@gmail.com>
2019-10-13 03:37:11 +0000

    Added nginx restart command

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # This manifest changes limit of allowed requests
 exec { 'increase allowed requests':
-    command => 'sed -i "s/15/15000/g" /etc/default/nginx',
+    command => 'sed -i "s/15/15000/g" /etc/default/nginx; sudo service nginx restart',
     path    => ['/usr/bin', '/sbin', '/bin', '/usr/sbin']
 }
